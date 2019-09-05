b95296d0cb08161bbec785c68a164f3a13a25e59
dario@tap-commerce.com <dario@tap-commerce.com>
2019-09-05 02:29:33 -0300

    test unit project and some test cases created

---
+++ XUnitTestProject1/CustomerControllerUnitTest.cs
@@ -0,0 +1,88 @@
+﻿
+
+using _2C2P___Aznar;
+using _2C2P___Aznar.Controllers;
+using _2C2P___Aznar.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+
+namespace XUnitTestProject1
+{
+    [TestClass]
+    public class CustomerControllerUnitTest
+    {
+
+        MockRepository repository;
+        CustomerController customerApi;
+        IMapper mapper;
+
+        [TestInitialize]
+        public void InitializeForTests()
+        {
+            repository = new MockRepository();
+
+
+            // Auto Mapper Configurations
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            mapper = mappingConfig.CreateMapper();
+
+            customerApi = new CustomerController(repository, mapper);
+        }
+
+        [TestMethod]
+        public async Task GetCustomer_Should_Return_Customer1()
+        {
+            IActionResult actionResult = await customerApi.Get(new InquiryCriteriaDto() { CustomerID = 1 });
+
+            Assert.IsNotNull(actionResult);
+
+            OkObjectResult result = actionResult as OkObjectResult;
+
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(((CustomerDto)result.Value).customerID, 1);
+        }
+
+        [TestMethod]
+        public async Task GetCustomer_Should_Customer1WithGetTwoTransactions()
+        {
+
+            IActionResult actionResult = await customerApi.Get(new InquiryCriteriaDto() { Email = "user@domain.com" });
+
+            Assert.IsNotNull(actionResult);
+
+            OkObjectResult result = actionResult as OkObjectResult;
+
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(((CustomerDto)result.Value).Transactions.Count, 2);
+
+
+
+        }
+
+
+
+        [TestMethod]
+        public async Task GetCustomer_Should_Return_NotFound()
+        {
+
+            IActionResult actionResult = await customerApi.Get(new InquiryCriteriaDto() { CustomerID = 1342424 });
+
+            Assert.IsNotNull(actionResult);
+
+            Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
+
+
+
+        }
+
+    }
+}
---
+++ XUnitTestProject1/MockRepository.cs
@@ -0,0 +1,55 @@
+﻿using _2C2P___Aznar.Models;
+using _2C2P___Aznar.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MockRepository : ICustomerRepository
+{
+    List<Customer> customers;
+
+    public bool FailGet { get; set; }
+
+    public MockRepository()
+    {
+        customers = new List<Customer>() {
+            new Customer(){customerID=1, Name="Customer1", Mobile="1992637281", Email="user@domain.com", Transactions=
+            new List<Transaction>(){ new Transaction() { Amount=125.25M, Currency=Transaction.TransactionCurrency.SGD, Date=DateTime.Now, Status=Transaction.TransactionStatus.Canceled, TransactionID=1} 
+            ,new Transaction() { Amount=225.25M, Currency=Transaction.TransactionCurrency.USD, Date=DateTime.Now, Status=Transaction.TransactionStatus.Success, TransactionID=2} }
+
+            
+            },
+            new Customer(){customerID=2, Name="Customer2", Mobile="9172735171", Email="user2@domain.com"},
+            new Customer(){customerID=3, Name="Customer3", Mobile="8361910353",Email="user3@domain.com" },
+            new Customer(){customerID=4, Name="Customer4", Mobile="7801274518", Email="user4@domain.com"}
+        };
+    }
+
+
+    public async Task<Customer> Find(long key)
+    {
+        if (FailGet)
+        {
+            throw new InvalidOperationException();
+        }
+        await Task.Delay(1000);
+        return   customers.Where(x=>x.customerID==key).FirstOrDefault();
+
+    }
+
+    public async Task<Customer> FindByEmail(string email)
+    {
+        if (FailGet)
+        {
+            throw new InvalidOperationException();
+        }
+        await Task.Delay(1000);
+
+        var customer = customers.Where(x => x.Email == email).FirstOrDefault();
+
+        return customer;
+
+    }
+}
